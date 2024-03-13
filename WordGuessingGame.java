9a05a9f219af3148247ea029b0275dfda5602ba4
rodri <202100213@alunos.ips.pt>
2024-03-13 08:57:02 +0000

    nivel 1.

---
+++ WordGuessingGame.java
@@ -0,0 +1,43 @@
+/**
+ * Write a description of class WordGuessingGame here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WordGuessingGame{
+    private String hiddenWord="abc";
+    private String guessedWord="___";
+    private int numberOfTries=0;
+    
+    public WordGuessingGame(){
+
+    }
+
+    public String getHiddenWord() {
+        return hiddenWord;
+    }
+
+    public void setHiddenWord(String hiddenWord) {
+        this.hiddenWord = hiddenWord;
+    }
+
+    public String getGuessedWord() {
+        return guessedWord;
+    }
+
+    public void setGuessedWord(String guessedWord) {
+        this.guessedWord = guessedWord;
+    }
+
+    public int getNumberOfTries() {
+        return numberOfTries;
+    }
+
+    public void setNumberOfTries(int numberOfTries) {
+        this.numberOfTries = numberOfTries;
+    }
+
+    public String showGuessedWord(){
+        return "Palavra escondida : "+guessedWord;
+    }
+}
